16e4ba0f9c75102cf7016a967317c717e11101ba
YuriyJS <45294411+YuriyJS@users.noreply.github.com>
2025-03-03 21:54:26 +0300

    contact section and scrollTotop
    html
    css
    js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,98 @@
-//Services modal active
+// Header effect scroll
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function () {
+  header.classList.toggle('sticky', window.scrollY > 1);
+});
+
+// Nav Menu items active
+
+// window.addEventListener('scroll', function () {
+//   const sections = this.document.querySelectorAll('section');
+//   const scrollY = this.window.scrollY;
+
+//   sections.forEach(current => {
+//     let sectionHeight = current.offsetHeight;
+//     let sectionTop = current.offsetTop - 50;
+//     let sectionId = current.getAttribute('id');
+
+//     if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document.querySelector(`.nav-menu a[href*="${sectionId}"]`).classList.add('active');
+//     } else {
+//       document.querySelector(`.nav-menu a[href*="${sectionId}"]`).classList.remove('active');
+//     }
+//   });
+// });
+  
+
+let isScrolling = false;
+
+window.addEventListener('scroll', function () {
+  if (!isScrolling) {
+    window.requestAnimationFrame(() => {
+      const sections = document.querySelectorAll('section');
+      const scrollY = window.scrollY;
+
+      sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute('id');
+
+        const navLink = document.querySelector(`.nav-menu a[href*="${sectionId}"]`);
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          if (navLink) navLink.classList.add('active');
+        } else {
+          if (navLink) navLink.classList.remove('active');
+        }
+      });
+
+      isScrolling = false;
+    });
+
+    isScrolling = true;
+  }
+});
+
+// Scroll to top button
+
+const scrollTotop = document.querySelector('.scrollTotop');
+
+
+// window.addEventListener('scroll', function () {
+//   if(window.scrollY > 500) {
+//     scrollTotop.classList.add('active');
+//   } else {
+//     scrollTotop.classList.remove('active');
+//   }
+// });
+
+//Сокращенный вариант c toggle
+
+window.addEventListener('scroll', function () {
+  scrollTotop.classList.toggle('active', window.scrollY > 500);
+});
+  
+
+scrollTotop.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// Services modal active
 
 const serviceModal = document.querySelectorAll('.service-modal');
 const learnMoreBtn = document.querySelectorAll('.learn-more-btn');
